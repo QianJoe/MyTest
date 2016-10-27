30d89833a6f683fe372e24ab492d6016fddf2bd1
QianJoe <joeqian@aliyun.com>
2016-10-27 11:28:10 +0800

    Initial Commit

---
+++ SchoolOrderFood/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SchoolOrderFood
+//
+//  Created by 乔谦 on 16/10/27.
+//  Copyright © 2016年 wlr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SchoolOrderFood/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SchoolOrderFood
+//
+//  Created by 乔谦 on 16/10/27.
+//  Copyright © 2016年 wlr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
